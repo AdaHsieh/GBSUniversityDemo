8c15bdcc0246dd7c606087c1ac723819679bf714
jesseko0820 <jesse.ko@gmail.com>
2019-04-11 14:26:51 +0800

    Update HelloWorldApplication.java

--- src/main/java/com/ibm/tw/demo/HelloWorldApplication.java
+++ src/main/java/com/ibm/tw/demo/HelloWorldApplication.java
@@ -13,7 +13,7 @@ public class HelloWorldApplication {
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
-		return "Hello World !";
+		return "Hello World ! Jesse ";
 	}
 
 	public static void main(String[] args) {
